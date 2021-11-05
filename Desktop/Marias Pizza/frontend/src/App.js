96a8192a4b4cc631ec072015751497365738eac9
Manoj Pun <manoj.haki@gmail.com>
2021-11-05 17:09:58 -0400

    first

---
+++ Desktop/Marias Pizza/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Home from './pages/Home';
+import { BrowserRouter } from 'react-router-dom';
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+    
+        <div>
+          <Home />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
---
+++ Desktop/Marias Pizza/frontend/src/pages/Test.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import React from "react";
+
+const baseURL = "http://localhost:4000/";
+
+export default function Test() {
+    const [post, setPost] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setPost(response.data);
+        });
+    }, []);
+    console.log("First test", post);
+
+
+
+    return (
+        <div>
+            <h1>Testinggggg</h1>
+
+        </div>
+    );
+}
