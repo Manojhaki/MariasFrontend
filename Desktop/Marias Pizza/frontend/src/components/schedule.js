86501836cbe1f10c19bd6c1bb8f35fb74caf1f5e
Manoj Pun <manoj.haki@gmail.com>
2021-11-09 14:25:24 -0500

    delete

--- Desktop/Marias Pizza/frontend/src/components/schedule.js
+++
@@ -1,9 +0,0 @@
-var checkTime = function () {
-    var today = weekday[now.getDay()];
-    var timeDiv = document.getElementById('timeDiv');
-    var dayOfWeek = now.getDay();
-    var hour = now.getHours();
-    var minutes = now.getMinutes();
-}
-
-console.log("check:", checktime.today);
